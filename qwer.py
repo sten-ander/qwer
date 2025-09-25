796119407c460920016d077c3a781385064ba159
opilane <opilane@HP-CZC2349HT2>
2025-09-25 11:50:41 +0300

    rwe

--- qwer.py
+++ qwer.py
@@ -1,1 +1,37 @@
-print("Hello!")
+# print("Tere, maailm!")
+# nimi = input("Mis su nimi on? ").capitalize()
+# print(f"Tere, maailm! Tervitan sind {nimi}")
+# vanus = int(input("Kui vana sa oled? "))
+# print(f"Tere,maailm! Tervitan sind {nimi}! Sa oled {vanus} aastat vana.".upper())
+
+# vanus=18
+# print(f"muutuja vanus on tüübilt {type(vanus)}")
+# eesnimi="Jaak"
+# print(f"muutuja eesnimi on tüübilt {type(eesnimi)}")
+# pikkus=1.65
+# print(f"muutuja pikkus on tüübilt {type(pikkus)}")
+# kas_käib_koolis=True
+# print(f"muutuja kas_käib_koolis on tüübilt {type(kas_käib_koolis)}")
+
+# from random import *
+# kommide_arv=randint(10, 100)
+# print(f"laual on {kommide_arv} kommi.")
+# võetud_kommide_arv=int(input("Mitu kommi sa soovid laualt ära võtta?"))
+# if võetud_kommide_arv>kommide_arv:
+#     print("Laual pole nii palju komme.")
+# else:
+#         kommide_arv-=võetud_kommide_arv
+#         print(f"nüüd on laual {kommide_arv} kommi.")
+
+# puu = input("Mis on puu ümbermõõt?")
+# print(f"puu läbimõõt on {int(puu)/3.14}")
+
+# N = input("Mis on ristküliku N külje pikkus?")
+# M = input("Mis on ristküliku M külje pikkus?")
+# print(f"Ristküliku pindala on {int(N)*int(M)}")
+
+# aeg = float(input("Mitu tundi kulus sõiduks? "))
+# teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+# kiirus = teepikkus/aeg
+
+# print("Sinu kiirus oli " + str(kiirus) + " km/h")
---
+++ rewq.py
@@ -0,0 +1,6 @@
+print("Saame tutavaks!")
+nimi = input("Mis su nimi on?").capitalize() # sõne
+print("Tere, " + nimi + "!")
+vanus = int(input("Kui vana sa oled? ")) #täisarv
+print("Oled " + str(vanus) + " aastat vana.")
+print(f"Järgmine aasta sa oled {vanus + 1} aastat vana.")
